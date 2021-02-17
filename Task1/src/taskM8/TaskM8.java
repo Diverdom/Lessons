4404d67dae3e0410d54f03948e404171b4003605
Diverdom <diverdom@gmail.com>
2021-02-17 20:45:07 +0300

    1

--- Task1/src/taskM8/TaskM8.java
+++ Task1/src/taskM8/TaskM8.java
@@ -8,16 +8,14 @@ public class TaskM8 {
         Random rand = new Random();
         int x = rand.nextInt();
         int huinya = x % 10;
-        System.out.println(huinya);
 
-        System.out.println(x);
         if (huinya == 1) {
             System.out.println(x + " рубль");
         }
-        if (huinya == 5 && huinya == 6 && huinya == 7 && huinya == 8 && huinya == 9) {
+        if (huinya == 5 || huinya == 6 || huinya == 7 || huinya == 8 || huinya == 9) {
             System.out.println(x + " рублей");
         }
-        if (x % 10 == 2 && x % 10 == 3 && x % 10 == 4) {
+        if (x % 10 == 2 || x % 10 == 3 || x % 10 == 4) {
             System.out.println(x + " рубля");
         }
     }
