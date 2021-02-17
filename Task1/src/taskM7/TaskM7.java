5d3da636abd3acd2c8a311c5f18167715d62659a
Diverdom <diverdom@gmail.com>
2021-02-17 22:50:32 +0300

    1

--- Task1/src/taskM7/TaskM7.java
+++ Task1/src/taskM7/TaskM7.java
@@ -7,14 +7,14 @@ import static java.lang.Math.*;
 public class TaskM7 {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the dimensions of the sides of the hole:");
-        int a = input.nextInt();
-        int b = input.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
         System.out.println("Enter the radius of the cap:");
-        int c = input.nextInt();
+        int c = scanner.nextInt();
 
         double x = sqrt(pow(a, 2) + pow(b, 2));
         //System.out.println(x);
@@ -24,5 +24,6 @@ public class TaskM7 {
         } else {
             System.out.println("No, the hole will not be covered");
         }
+        scanner.close();
     }
 }
