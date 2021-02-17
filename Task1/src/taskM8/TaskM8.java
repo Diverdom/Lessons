00f3d5764e3fd5d1b4e661f9fa9dea0bb5f52038
Diverdom <diverdom@gmail.com>
2021-02-17 20:36:02 +0300

    1

---
+++ Task1/src/taskM8/TaskM8.java
@@ -0,0 +1,24 @@
+package taskM8;
+
+import java.util.Random;
+
+public class TaskM8 {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int x = rand.nextInt();
+        int huinya = x % 10;
+        System.out.println(huinya);
+
+        System.out.println(x);
+        if (huinya == 1) {
+            System.out.println(x + " рубль");
+        }
+        if (huinya == 5 && huinya == 6 && huinya == 7 && huinya == 8 && huinya == 9) {
+            System.out.println(x + " рублей");
+        }
+        if (x % 10 == 2 && x % 10 == 3 && x % 10 == 4) {
+            System.out.println(x + " рубля");
+        }
+    }
+}
