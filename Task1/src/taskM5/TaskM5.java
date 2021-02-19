ff09fca9c9dc62948d6a47763c07580db96f3f0a
Diverdom <diverdom@gmail.com>
2021-02-19 21:41:07 +0300

    1

--- Task1/src/taskM5/TaskM5.java
+++ Task1/src/taskM5/TaskM5.java
@@ -10,6 +10,29 @@ public class TaskM5 {
 
     public static void main(String[] args) {
 
+        System.out.println("Enter four-digit number");
+        Scanner scanner = new Scanner(System.in);
 
+        int number = scanner.nextInt();
+        int first, second, third, four;
+        boolean result;
+
+        first = (number % 10); //получили первую цифру через остаток от деления на 10
+        number /= 10; //откинули последний знак в числе и присвоили новое значение перенной
+
+        second = number % 10; //получили вторую цифру через остаток от деления на 10
+        number /= 10; //откинули второй знак из числа и присвоили новое значение переменной
+
+        third = number % 10; //получили третью цифру через остаток от деления на 10
+        number /= 10;//откинули третий знак из числа и присвоили новое значение переменной
+
+        four = number % 10; //получили четвертую цифру через остаток от деления на 10
+
+        result = (first != second && first != third && first != four && second != third
+                && second != four && third != four); // определяем уникальность цифр в числе
+
+        System.out.println(result); //выводим булево значение
+
+        scanner.close();
     }
 }
