dba4a3854679cd8ed30939ee14a3192bc1bd702e
Diverdom <diverdom@gmail.com>
2021-03-02 20:02:49 +0300

    Задача 9

---
+++ Task1/src/taskM9/TaskM9.java
@@ -0,0 +1,21 @@
+package taskM9;
+
+/* Задание 9
+Изменить пример с суммой чисел таким образом, чтобы рассчитывалась не сумма, а
+произведение, т.е. факториал числа.
+*/
+
+
+public class TaskM9 {
+
+
+    public static void main(String[] args) {
+        int number = 6;
+        long factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial = factorial * i;
+
+        }
+        System.out.println("Factorial is :" + factorial);
+    }
+}
