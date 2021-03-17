27792e6175939db9a66f1bd57d569381b97b8b89
Diverdom <diverdom@gmail.com>
2021-03-17 20:40:06 +0300

    TaskM13 DONE

---
+++ Task1/src/taskM13/TaskM13.java
@@ -0,0 +1,27 @@
+package taskM13;
+
+import java.util.Random;
+
+public class TaskM13 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int array[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = random.nextInt(100);
+        }
+
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int index = 0; index < array.length - 1 - i; index++) {
+                if (array [index] > array [index + 1]) {
+                    int temp = array [index];
+                    array [index] = array [(index + 1)];
+                    array [(index + 1)] = temp;
+                }
+            }
+        }
+        System.out.println("Наибольшее значение массива = " +array [9]);
+
+        }
+    }
+
